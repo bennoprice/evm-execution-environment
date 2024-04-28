b418809f8d81f7444121c4181c77e5bf44099c3d
Ben Price <ben.jules.price@gmail.com>
2024-04-28 02:40:16 +0100

    feat: benchmarking
    - support contract deployment
    - support contract benchmarking

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 
+import * as dotenv from "dotenv"
+dotenv.config({ path: ".env" })
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+if (!PRIVATE_KEY)
+  throw new Error("Missing PRIVATE_KEY in .env");
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
@@ -8,6 +15,11 @@ const config: HardhatUserConfig = {
       forking: {
         url: "https://eth.llamarpc.com"
       }
+    },
+    opsepolia: {
+      url: "https://optimism-sepolia.blockpi.network/v1/rpc/public",
+      chainId: 11155420,
+      accounts: [PRIVATE_KEY]
     }
   }
 };
--- scripts/run.ts
+++ scripts/run.ts
@@ -2,13 +2,30 @@ import { ethers } from "hardhat";
 
 const main = async () => {
   const [signer] = await ethers.getSigners();
-  const nonce = await signer.getNonce();
+  //const nonce = await signer.getNonce();
 
-  const address = ethers.getCreateAddress({ from: signer.address, nonce });
-  await signer.sendTransaction({ data: "0x6300000005630000000a81810163000000010180630000000055" });
-  const output = await ethers.provider.getStorage(address, 0);
+  //const address = ethers.getCreateAddress({ from: signer.address, nonce });
 
-  console.log(output);
+  const deploy = await signer.sendTransaction({ data: "0x630000007980600E6000396000F36004361063000000335760003560E01C806371e44efa1415630000003257630000003863000000043560F81C630000003A565B5B600080FD5B005B6000819050600063000000538263000000026300000070565B90506300000062816300000066565B5050565B8063000000005550565B8082029350505056" });
+  const receipt = await deploy.wait();
+  const address = receipt!.contractAddress!;
+  console.log("contract address:", address);
+
+  const attrBefore = await signer.provider.getStorage(address, 0);
+  console.log("attr before:", attrBefore);
+
+  const call = await signer.sendTransaction({
+    to: address,
+    data: "0x71e44efa01"
+  });
+  console.log("call txhash:", call.hash);
+
+  const attrAfter = await signer.provider.getStorage(address, 0);
+  console.log("attr after:", attrAfter);
+
+  //const output = await ethers.provider.getStorage(address, 0);
+
+  //console.log(output);
 };
 
 main().catch((error) => {
